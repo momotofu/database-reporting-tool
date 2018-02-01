46df7b018d67af630d0117653e5381cedd017261
Christopher Reece <mrkita.reece@gmail.com>
2018-02-01 19:52:38 +0900

    planned program structure

---
+++ print_report.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+"""
+1. define a function for each query
+2. print information neatly into console
+"""
+
+def top_three_articles():
+    pass
+
+def list_authors_by_popularity():
+    pass
+
+def requests_days_with_errors():
+    pass
