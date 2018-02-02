1bcde19d4fabcd75e70c76c02a1a4e58eac8c93e
Christopher Reece <mrkita.reece@gmail.com>
2018-02-02 12:58:05 +0900

    implimented top_three_articles

--- print_report.py
+++ print_report.py
@@ -4,12 +4,23 @@ import psycopg2
 1. define a function for each query
 2. print information neatly into console
 """
+DBNAME = 'news'
 
 def top_three_articles():
-    pass
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    cursor.execute("""
+        select count(*) as views, replace(replace(path, '/article/', ''), '-', ' ')
+        as title from log group by path order by views desc offset 1 limit 3;
+    """)
+    top_three = cursor.fetchall()
+    db.close()
+    return top_three
 
 def list_authors_by_popularity():
     pass
 
 def requests_days_with_errors():
     pass
+
+print(top_three_articles())
