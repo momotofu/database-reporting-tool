827ded4eeebfd621072c21cf03af8ff6770d97e5
Christopher Reece <mrkita.reece@gmail.com>
2018-02-08 19:42:32 +0900

    added main logic

--- print_report.py
+++ print_report.py
@@ -37,44 +37,45 @@ def execute_query(query, connection):
     return result
 
 
-print_result_table(
-    'Top three articles: ',
-    ['views', 'article title'],
-    execute_query("""
-        SELECT COUNT(*) as VIEWS, REPLACE(REPLACE(path, '/article/', ''),
-        '-', ' ') AS title
-            FROM log
-            GROUP BY path
-            ORDER by views DESC
-            OFFSET 1 LIMIT 3;
-    """, connect(DBNAME))
-    )
-print_result_table(
-    'Authors by popularity: ',
-    ['author name', 'views'],
-    execute_query("""
-        SELECT name, SUM(views) AS views
-            FROM (SELECT author, views
-                FROM (SELECT COUNT(*) AS views, REPLACE(path, '/article/', '')
-                AS slug
-                    FROM log
-                    GROUP BY path
-                    ORDER BY views DESC OFFSET 1) AS A, articles
-                WHERE A.slug = articles.slug) AS B, authors
-            WHERE B.author = authors.id
-            GROUP BY name
-            ORDER BY views DESC;
-    """, connect(DBNAME))
-    )
-print_result_table(
-    'Days with errors over 1%: ',
-    ['day', 'error percentage'],
-    execute_query("""
-        SELECT day, ROUND(percentage, 2) AS percentage
-            FROM (SELECT B.day, (1.0 * b_occurances / occurances * 100) AS
-            percentage
-                FROM good_status_by_day AS A, bad_status_by_day AS B
-                WHERE A.day = B.day) AS subq
-             WHERE percentage > 1;
+if __name__ == '__main__':
+    print_result_table(
+        'Top three articles: ',
+        ['views', 'article title'],
+        execute_query("""
+            SELECT COUNT(*) as VIEWS, REPLACE(REPLACE(path, '/article/', ''),
+            '-', ' ') AS title
+                FROM log
+                GROUP BY path
+                ORDER by views DESC
+                OFFSET 1 LIMIT 3;
         """, connect(DBNAME))
-    )
+        )
+    print_result_table(
+        'Authors by popularity: ',
+        ['author name', 'views'],
+        execute_query("""
+            SELECT name, SUM(views) AS views
+                FROM (SELECT author, views
+                    FROM (SELECT COUNT(*) AS views,
+                        REPLACE(path, '/article/', '') AS slug
+                        FROM log
+                        GROUP BY path
+                        ORDER BY views DESC OFFSET 1) AS A, articles
+                    WHERE A.slug = articles.slug) AS B, authors
+                WHERE B.author = authors.id
+                GROUP BY name
+                ORDER BY views DESC;
+        """, connect(DBNAME))
+        )
+    print_result_table(
+        'Days with errors over 1%: ',
+        ['day', 'error percentage'],
+        execute_query("""
+            SELECT day, ROUND(percentage, 2) AS percentage
+                FROM (SELECT B.day, (1.0 * b_occurances / occurances * 100) AS
+                percentage
+                    FROM good_status_by_day AS A, bad_status_by_day AS B
+                    WHERE A.day = B.day) AS subq
+                 WHERE percentage > 1;
+            """, connect(DBNAME))
+        )
